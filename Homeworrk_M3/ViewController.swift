ea2f3732c218ca453335b0462eb1cd77f39f4cd7
Aziza <atabaevaa243@gmail.com>
2022-12-23 18:28:25 +0600

    Initial Commit

---
+++ Homeworrk_M3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homeworrk_M3
+//
+//  Created by mac on 23/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
